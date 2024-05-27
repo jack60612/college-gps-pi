b8812941e9e55d8bb6ca7d320e5e833e90b5221d
Jack Nelson <jack@jacknelson.xyz>
2024-05-27 00:18:56 -0400

    Update page.py

--- gpspi/types/page.py
+++ gpspi/types/page.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class Page(Enum):
-    TIME_AND_SATELLITES: int = 1
-    GPS_COORDINATES: int = 2
-    SELECT_DESTINATION: int = 3
-    SELECT_WAYPOINTS: int = 4
-    COMPASS_HEADING_AND_SPEED: int = 5
+    TIME_AND_SATELLITES: int = 0
+    GPS_COORDINATES: int = 1
+    SELECT_DESTINATION: int = 2
+    SELECT_WAYPOINTS: int = 3
+    COMPASS_HEADING_AND_SPEED: int = 4
