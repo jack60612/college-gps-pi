ea9e3d8ccc109ec153a1c615c86e7363306a9dc0
Jack Nelson <jack@jacknelson.xyz>
2024-06-01 13:43:28 -0400

    add more import code

--- gpspi/mapping/importer.py
+++ gpspi/mapping/importer.py
@@ -1,18 +1,54 @@
 """This file converts it from extracted OSM data to the osmnx format"""
 
-source_path = "north-america-all-roads.osm.pbf"
+#source_path = "north-america-all-roads.osm.pbf"
+source_path = "florida-all-roads.osm.pbf"
 
+import datetime
 import osmnx as ox
 from networkx import MultiDiGraph
 from pyrosm import OSM
 
 
+def miles_to_degrees(miles):
+    """Convert miles to degrees (approximately)."""
+    return miles / 69.0
+
+def get_bounding_box(lat, lon, radius_miles=50) -> tuple[float,float,float,float]:
+    """
+    Returns a bounding box representing a circle of a given radius (in miles) around a coordinate point.
+
+    Parameters:
+    - lat (float): Latitude of the center point.
+    - lon (float): Longitude of the center point.
+    - radius_miles (float): Radius of the circle in miles. Default is 50 miles.
+
+    Returns:
+    - tuple: A tuple containing the min lat and min long then max lat and max long
+    """
+    radius_degrees = miles_to_degrees(radius_miles)
+    min_lat = lat - radius_degrees
+    max_lat = lat + radius_degrees
+    min_lon = lon - radius_degrees
+    max_lon = lon + radius_degrees
+    
+    return (min_lon, min_lat, max_lon, max_lat)
+
+
 def get_graph(file_name: str) -> MultiDiGraph:
+    
+    b_box = list(get_bounding_box(27.994402, -81.760254, 50))
+    
+    
+    #osm = OSM(file_name, b_box)
     osm = OSM(file_name)
     print("Getting driving network")
-    nodes, edges = osm.get_network(network_type="driving")
+    time = datetime.datetime.now()
+    nodes, edges = osm.get_network(nodes=True,network_type="driving")
+    print(f"Time taken: {datetime.datetime.now() - time}")
     print("Converting to graphml")
-    graph = osm.to_graph(nodes, edges, network_type="networkx")
+    time = datetime.datetime.now()
+    graph = osm.to_graph(nodes, edges, graph_type="networkx")
+    print(f"Time taken: {datetime.datetime.now() - time}")
     
     return graph
 
